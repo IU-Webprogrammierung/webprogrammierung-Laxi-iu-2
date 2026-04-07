46e969925309921150ccccafce48b442027aa5d0
Laxshana Gnanananthan <laxshana.gnanananthan@iu-study.org>
2026-04-07 17:38:54 +0200

    adding code to javscript

---
+++ script.js
@@ -0,0 +1,13 @@
+const counter = document.getElementById("counter");
+let start = 0;
+let end = 24;
+
+function updateCounter() {
+    if (start < end) {
+        start++;
+        counter.textContent = start;
+        setTimeout(updateCounter, 195);
+    }
+}
+
+updateCounter();
