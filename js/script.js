65d4e5220d71aa2802caa1955f87b4fffbfb5a91
Laxshana Gnanananthan <laxshana.gnanananthan@iu-study.org>
2026-04-07 17:57:36 +0200

    fixing javacript error; file was not found

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const counter = document.getElementById("counter");
+if (counter) {
+    let start = 0;
+    let end = 24;
+
+    function updateCounter() {
+        if (start < end) {
+            start++;
+            counter.textContent = start;
+            setTimeout(updateCounter, 80);
+        }
+    }
+    updateCounter();
+}
